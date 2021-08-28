35c20c198583121638745196ac4528957b934bc6
Anuj Raval <anujraval24@gmail.com>
2021-08-28 22:01:23 +0530

    feat: update folder structure

--- server/src/config/dbConnection.js
+++ server/src/config/dbConnection.js
@@ -4,16 +4,10 @@ import { redBright, greenBright, yellowBright } from 'chalk';
 
 // Database Name & URL
 const DATABASE_NAME = process.env.DATABASE_NAME;
-console.log(`CONNECTION_URL`, process.env.CONNECTION_URL + DATABASE_NAME )
-export const CONNECTION_URL = process.env.CONNECTION_URL + DATABASE_NAME + '?authSource=admin&ssl=false';
+export const CONNECTION_URL = process.env.CONNECTION_URL + DATABASE_NAME;
 
 (async () => {
 	try {
-		set('useCreateIndex', true);
-
-		//for making use of findOneAndUpdate else it will not work
-		set('useFindAndModify', false);
-
 		//Connection establishment
 		connect(CONNECTION_URL, {
 			useNewUrlParser: true,
